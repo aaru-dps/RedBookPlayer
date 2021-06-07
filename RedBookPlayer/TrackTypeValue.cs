30eab90dfb0616de1480a96f8841bb46739d2270
Matt Nadareski <mnadareski@outlook.com>
2021-06-06 21:41:38 -0700

    Separate out TrackTypeValue

---
+++ RedBookPlayer/TrackTypeValue.cs
@@ -0,0 +1,12 @@
+namespace RedBookPlayer
+{
+    /// <summary>
+    /// Track type for determining how to handle
+    /// </summary>
+    /// <remarks>Does not distinguish between different data track types</remarks>
+    public enum TrackTypeValue
+    {
+        Audio,
+        Data
+    }
+}
